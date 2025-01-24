4adccbbd24adb51c93d944551d768055c8fabbc1
DamCo <73948784+Damdam86@users.noreply.github.com>
2025-01-24 15:58:10 +0100

    modif

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Charger le fichier CSV
-url = 'https://raw.githubusercontent.com/Damdam86/WildTech/refs/heads/main/merged_df.csv'
-df = pd.read_csv(url, sep=';')
+url = 'https://wildstartech.s3.eu-west-3.amazonaws.com/merged_df.csv'
+df = pd.read_csv('url', sep=';')
 
 # Ajouter un sélecteur pour choisir une entreprise
 selected_company = st.selectbox("Sélectionnez une entreprise :", df['nom'].dropna().unique())
